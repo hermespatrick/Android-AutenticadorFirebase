695fc4d0df8f1d53f1dbcfe25e902a6b6ae17c0d
Patrick Nunez <panu0418@netcracker.com>
2020-02-07 11:44:45 -0500

    added user info registration - firestore

--- app/src/main/java/com/devotees/authenticatorapp/activity_register.java
+++ app/src/main/java/com/devotees/authenticatorapp/activity_register.java
@@ -6,25 +6,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class activity_register extends AppCompatActivity {
 
+    public static final String TAG = "TAG";
     EditText mFullName,mEmail,mPassword,mPhone;
+    Spinner mSpinner;
     Button mRegisterBtn;
     TextView mLoginBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +47,12 @@ public class activity_register extends AppCompatActivity {
         mEmail=findViewById(R.id.email);
         mPassword=findViewById(R.id.password);
         mPhone=findViewById(R.id.phone);
+        mSpinner=findViewById(R.id.spinner2);
         mRegisterBtn=findViewById(R.id.registerBtn);
         mLoginBtn=findViewById(R.id.createTextLoginBtn);
 
         fAuth=FirebaseAuth.getInstance();
+        fStore=FirebaseFirestore.getInstance();
         progressBar=findViewById(R.id.progressBar);
 
         if(fAuth.getCurrentUser()!=null){
@@ -49,8 +63,11 @@ public class activity_register extends AppCompatActivity {
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email=mEmail.getText().toString().trim();
+                final String email=mEmail.getText().toString().trim();
                 String password=mPassword.getText().toString().trim();
+                final String fullname=mFullName.getText().toString();
+                final String phone=mPhone.getText().toString();
+
 
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is required");
@@ -76,6 +93,24 @@ public class activity_register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(activity_register.this,"User Created",Toast.LENGTH_SHORT).show();
+
+                            userID=fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference=fStore.collection("users").document(userID);
+
+                            Map<String,Object> user=new HashMap<>();
+                            user.put("fName",fullname);
+                            user.put("email",email);
+                            user.put("phone",phone);
+
+
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG,"onSuccess: user profile is created for "+userID);
+
+                                }
+                            });
+
                             startActivity(new Intent(getApplicationContext(),MainActivity.class));
                         }else{
                             Toast.makeText(activity_register.this, "Error !"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
