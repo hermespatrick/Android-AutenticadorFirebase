63f4f5f2b849d09078b04e63ea31497ade66b808
Patrick Nuñez <patricio181@hotmail.com>
2020-02-11 00:15:51 -0500

    rediseñando mi main activity layout

--- app/src/main/java/com/devotees/authenticatorapp/MainActivity.java
+++ app/src/main/java/com/devotees/authenticatorapp/MainActivity.java
@@ -5,15 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    TextView fullname,email,phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        phone=findViewById(R.id.textViewPhone);
+        fullname=findViewById(R.id.textViewName);
+        email=findViewById(R.id.textViewEmail);
     }
 
     public void logout(View view) {
